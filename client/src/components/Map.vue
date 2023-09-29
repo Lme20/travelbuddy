c2ff3d4e7fdc28c6629bdd3985c6a303dd81cd22
louisme <louisme@student.chalmers.se>
2023-09-29 18:31:03 +0200

    Changed path styling

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,33 +1,62 @@
 <template>
     <b-row>
-        <GmapMap :center='center' :zoom='10' style='width:100%; height:700px;'>
+        <GmapMap :center='center' :zoom='10' style='width:100%; height:700px;' @click="mapClicked">
+          <!-- Marker -->
           <GmapMarker
             :key="index"
             v-for="(m, index) in markers"
             :position="m.position"
-            @click="center=m.position"
+            :icon="customMarker"
+            @click="deleteMarker(index)"
           />
           <GmapAutocomplete
             @place_changed = 'setPlace'
+          />
+          <!-- Path -->
+          <GmapPolyline
+            :path="pathCoordinates"
+            :options="{ strokeColor: '#f87425', strokeOpacity: 0, strokeWeight: 3, icons: dottedLineIcons }"
           />
         </GmapMap>
     </b-row>
 </template>
 
 <script>
-import { Map as GmapMap, Marker as GmapMarker } from 'vue2-google-maps'
+/* global google */
+import { Map as GmapMap, Marker as GmapMarker, Polyline as GmapPolyline } from 'vue2-google-maps'
 import { EventBus } from '@/EventBus'
 // import { BButton } from 'bootstrap-vue'
 
 export default {
-  components: { GmapMap, GmapMarker },
+  components: { GmapMap, GmapMarker, GmapPolyline },
   name: 'GoogleMap',
   data() {
     return {
       center: { lat: 45.508, lng: -73.587 },
       currentPlace: null,
       markers: [],
-      places: []
+      places: [],
+      pathCoordinates: [],
+
+      // Marker styling
+      customMarker: {
+        url: require('@/assets/marker_icon.png'),
+        scaledSize: new google.maps.Size(20, 32)
+      },
+
+      // Path styling
+      dottedLineIcons: [
+        {
+          icon: {
+            // path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+            path: 'M 0,-1 0,1',
+            strokeOpacity: 0.8,
+            scale: 4
+          },
+          offset: '0',
+          repeat: '20px' // Distance between each dot
+        }
+      ]
     }
   },
   mounted() {
@@ -48,7 +77,20 @@ export default {
         EventBus.$emit('new-location', marker) // Emitting the event
         this.center = marker
         this.currentPlace = null
+        this.pathCoordinates.push(marker)
       }
+    },
+    deleteMarker(index) {
+      this.markers.splice(index, 1)
+      this.pathCoordinates.splice(index, 1)
+    },
+    mapClicked(event) {
+      const clickedPosition = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng()
+      }
+      this.markers.push({ position: clickedPosition })
+      this.pathCoordinates.push(clickedPosition)
     },
     geolocate: function () {
       navigator.geolocation.getCurrentPosition(position => {
