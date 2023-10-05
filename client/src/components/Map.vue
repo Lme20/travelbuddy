2c450616e1f32776034051c79bfbc839a683c1c1
louisme <louisme@student.chalmers.se>
2023-10-05 20:37:22 +0200

    fixed placing markers on map

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,104 +1,130 @@
 <template>
   <b-row>
-      <GmapMap v-if="center" :center='center' :zoom='10' style='width:100%; height:600px;' @click="mapClicked">
-        <!-- Marker -->
-        <GmapMarker
-          :key="index"
-          v-for="(m, index) in markers"
-          :position="m.position"
-          :icon="customMarker"
-          @click="deleteMarker(index)"
-        />
-        <GmapAutocomplete
-          @place_changed = 'setPlace'
-        />
+      <GmapMap ref="gmap" v-if="center" :center='center' :zoom='10' style='width:100%; height:600px;' >
+        <GmapAutocomplete />
         <!-- Path -->
-        <GmapPolyline
-          :path="pathCoordinates"
-          :options="{ strokeColor: '#f87425', strokeOpacity: 0, strokeWeight: 3, icons: dottedLineIcons }"
-        />
+        <GmapPolyline :options="{ strokeColor: '#f87425', strokeOpacity: 0, strokeWeight: 3 }"/>
       </GmapMap>
   </b-row>
 </template>
 
 <script>
 /* global google */
-import { Map as GmapMap, Marker as GmapMarker, Polyline as GmapPolyline } from 'vue2-google-maps'
-import { EventBus } from '@/EventBus'
-// import { BButton } from 'bootstrap-vue'
+import landmarkMarker from '@/assets/landmarkMarker.png'
+import facilityMarker from '@/assets/facilityMarker.png'
+import entertainmentMarker from '@/assets/entertainmentMarker.png'
+import defaultMarker from '@/assets/default-marker.png'
+import attractionMarker from '@/assets/attractionMarker.png'
+
+import { Map as GmapMap, Polyline as GmapPolyline } from 'vue2-google-maps'
+// import { EventBus } from '@/EventBus'
 
 export default {
-  components: { GmapMap, GmapMarker, GmapPolyline },
+  components: { GmapMap, GmapPolyline },
   name: 'GoogleMap',
   data() {
     return {
-      center: { lat: 45.508, lng: -73.587 },
-      currentPlace: null,
-      markers: [],
-      places: [],
-      pathCoordinates: [],
-
-      // Marker styling
-      customMarker: {
-        url: require('@/assets/marker_icon.png'),
-        scale: 1.5
-      },
-
-      // Path styling
-      dottedLineIcons: [
-        {
-          icon: {
-            path: 'M 0,-1 0,1',
-            strokeOpacity: 0.8,
-            scale: 4
-          },
-          offset: '0',
-          repeat: '30px' // Distance between each dot
-        }
-      ]
+      center: { lat: 57.7089, lng: 11.9746 }, // Latitude and Longitude object
+      radius: 50000, // Radius in meters
+      googleMapInstance: null,
+      markers: []
     }
   },
+  props: ['userLocation'],
+  watch: {
+
+  },
   mounted() {
-    this.geolocate()
-    this.customMarker.scaledSize = new google.maps.Size(21, 32)
+    this.$refs.gmap.$mapPromise.then((map) => {
+      this.googleMapInstance = map
+      google.maps.event.addListener(map, 'idle', () => {
+        this.googleMapInstance = this.$refs.gmap.$mapObject
+      })
+    })
   },
   methods: {
-    setPlace(place) {
-      this.currentPlace = place
-    },
-    addMarker() {
-      if (this.currentPlace && this.currentPlace.geometry) {
-        const marker = {
-          lat: this.currentPlace.geometry.location.lat(),
-          lng: this.currentPlace.geometry.location.lng(),
-          name: this.currentPlace.formatted_address
-        }
-        this.markers.push({ position: marker })
-        EventBus.$emit('new-location', marker) // Emitting the event
-        this.center = marker
-        this.currentPlace = null
-        this.pathCoordinates.push(marker)
+    fetchMarkers() {
+      console.log('Entering fetchMarkers')
+
+      if (!this.center || !this.radius) return // Changed this.userLocation to this.center
+
+      const service = new google.maps.places.PlacesService(this.googleMapInstance)
+
+      if (!this.googleMapInstance) {
+        console.error('Google Map instance not initialized')
+        return
       }
-    },
-    deleteMarker(index) {
-      this.markers.splice(index, 1)
-      this.pathCoordinates.splice(index, 1)
-    },
-    mapClicked(event) {
-      const clickedPosition = {
-        lat: event.latLng.lat(),
-        lng: event.latLng.lng()
+
+      const request = {
+        location: new google.maps.LatLng(this.center.lat, this.center.lng), // Explicitly make it LatLng
+        radius: this.radius,
+        type: ['church', 'city_hall', 'hindu_temple', 'mosque', 'airport', 'bus_station',
+          'taxi_stand', 'lodging', 'shopping_mall', 'store', 'supermarket', 'parking', 'ATM',
+          'movie_theater', 'casino', 'night_club', 'bar', 'aquarium', 'art_gallery', 'zoo',
+          'tourist_attraction', 'amusement_park', 'restaurant', 'park', 'museum']
       }
-      this.markers.push({ position: clickedPosition })
-      this.pathCoordinates.push(clickedPosition)
-    },
-    geolocate: function () {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
+
+      console.log('Radius:', this.radius) // Debug
+      console.log('Center:', this.center) // Debug
+
+      service.nearbySearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          this.placeMarkers(results)
         }
       })
+    },
+    placeMarkers(results) {
+      try {
+        console.log('Results: ', results) // Debug
+        // Clear existing markers
+        this.markers.forEach(marker => marker.setMap(null))
+        this.markers = []
+
+        results.forEach((result) => {
+          let markerIcon = defaultMarker
+
+          console.log('Marker Icon:', markerIcon) // Debug
+
+          // LANDMARK CATEGORY
+          if (result.types.includes('church') || result.types.includes('city_hall') || result.types.includes('hindu_temple') ||
+                  result.types.includes('mosque')) {
+            markerIcon = landmarkMarker
+            // FACILITY CATEGORY
+          } else if (result.types.includes('airport') || result.types.includes('bus_station') || result.types.includes('taxi_stand') ||
+                result.types.includes('lodging') || result.types.includes('shopping_mall') || result.types.includes('store') ||
+                result.types.includes('supermarket') || result.types.includes('parking') || result.types.includes('ATM') || result.types.includes('restaurant')) {
+            markerIcon = facilityMarker
+            // ENTERTAINMENT CATEGORY
+          } else if (result.types.includes('movie_theater') || result.types.includes('casino') || result.types.includes('night_club') ||
+                  result.types.includes('bar')) {
+            markerIcon = entertainmentMarker
+            // ATTRACTION CATEGORY
+          } else if (result.types.includes('park') || result.types.includes('aquarium') || result.types.includes('art_gallery') || result.types.includes('museum') ||
+                  result.types.includes('zoo') || result.types.includes('tourist_attraction') || result.types.includes('amusement_park')) {
+            markerIcon = attractionMarker
+          }
+          const marker = new google.maps.Marker({
+            position: result.geometry.location,
+            map: this.googleMapInstance,
+            icon: {
+              url: markerIcon,
+              scaledSize: new google.maps.Size(30, 45) // size to 25x25 pixels
+            }
+          })
+          console.log('Marker:', marker)
+          this.markers.push(marker) // Push to Array
+          console.log('Marker placed at: ', result.geometry.location) // Debug
+        })
+      } catch (error) {
+        console.error('Error in placeMarkers:', error)
+      }
+    },
+    updateLocation(newLocation) {
+      console.log('Received newLocation:', newLocation)
+      this.center = newLocation // { lat: ..., lng: ... }
+      this.radius = 50000 // 50 km in meters
+      this.fetchMarkers()
+      console.log('newLocation and marker fecthed:', newLocation, this.fetchMarkers)
     }
   }
 }
