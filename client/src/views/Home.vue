7dd1becc147a124abb6c6083fd40944ab73511e5
benhede <benhede@pm.me>
2023-10-14 16:57:35 +0200

    modal up (buggy user list updates though)

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,35 +2,38 @@
   <div class="home-container" :style="{ backgroundImage: `url(${backgroundImage})` }">
     <div class="hero-section">
       <h1>EXPLORE THE WORLD</h1>
-      <b-button @click="scrollToMap" class="button-style" >Find my destination</b-button>
+      <b-button @click="scrollToMap" class="button-style">Find my destination</b-button>
+      <!-- <user-modal :options="showModal"/> -->
+      <b-modal id="user-modal" v-model="show" hide-footer>
+        <template #modal-title>
+            Create new user
+        </template>
+        <user-entry/>
+        <b-button class="mt-3" block @click="$bvModal.hide('user-modal').then(getUsers())">Close</b-button>
+    </b-modal>
     </div>
     <b-container fluid>
       <!-- Input and Button for Location -->
-        <b-form @submit.prevent="addLocation">
-          <b-form-group
-            label="Enter your destination"
-            description="placeholder text"
-            label-cols-sm="4"
-            label-cols-lg="3"
-            content-cols-sm
-            content-cols-lg="7">
-            <div class="d-flex align-items-center">
-              <!-- Create location request -->
-              <b-input v-model="userLocation" placeholder="Where are you going?" size="sm" class="search-box"></b-input>
-              <b-button type="submit">Confirm Location</b-button>
-              <!-- Create journal request -->
-              <b-button @click="contentToDisplay = 1">Create Journal</b-button>
-              <!-- Create checklist request -->
-              <b-button @click="contentToDisplay = 2">Create Checklist</b-button>
-            </div>
-          </b-form-group>
-        </b-form>
+      <b-form @submit.prevent="addLocation">
+        <b-form-group label="Enter your destination" description="placeholder text" label-cols-sm="4" label-cols-lg="3"
+          content-cols-sm content-cols-lg="7">
+          <div class="d-flex align-items-center">
+            <!-- Create location request -->
+            <b-input v-model="userLocation" placeholder="Where are you going?" size="sm" class="search-box"></b-input>
+            <b-button type="submit">Confirm Location</b-button>
+            <!-- Create journal request -->
+            <b-button @click="contentToDisplay = 1">Create Journal</b-button>
+            <!-- Create checklist request -->
+            <b-button @click="contentToDisplay = 2">Create Checklist</b-button>
+          </div>
+        </b-form-group>
+      </b-form>
       <b-row class="h-100">
         <b-col lg="12" md="12" sm="12" class="h-100">
           <!-- Sidebar -->
           <right-sidebar :contentToDisplay="contentToDisplay" />
           <!-- Google map -->
-        <GoogleMap ref="googleMapRef" :userLocation="userLocation" v-if="showMap"/>
+          <GoogleMap ref="googleMapRef" :userLocation="userLocation" v-if="showMap" />
         </b-col>
       </b-row>
     </b-container>
@@ -42,6 +45,7 @@ import { Api } from '@/Api'
 import GoogleMap from '@/components/Map.vue'
 import axios from 'axios'
 import RightSidebar from '@/components/RightSidebar.vue'
+import UserEntry from '@/components/UserEntry.vue'
 
 export default {
   name: 'home',
@@ -50,12 +54,36 @@ export default {
       userLocation: null,
       message: 'none',
       showMap: false,
+      showModal: false,
       backgroundImage: '',
       contentToDisplay: null,
-      isButtonActive: false
+      isButtonActive: false,
+      users: []
     }
   },
+  // Fetch background image when component is created
+  created() {
+    this.fetchImage()
+  },
+  mounted() {
+    this.getUsers()
+  },
+  components: {
+    'right-sidebar': RightSidebar,
+    'user-entry': UserEntry,
+    GoogleMap
+  },
   methods: {
+    getUsers() {
+      Api.get('/users')
+        .then(response => {
+          this.users = response.data.users
+        })
+        .catch(error => {
+          this.users = []
+          console.log(error)
+        })
+    },
     getMessage() {
       Api.get('/')
         .then(response => {
@@ -88,11 +116,17 @@ export default {
       }
     },
     scrollToMap() {
-      this.showMap = true
-      this.$nextTick(() => {
-        const mapElement = this.$refs.googleMapRef.$el
-        mapElement.scrollIntoView({ behavior: 'smooth' })
-      })
+      this.getUsers()
+      if (this.users.length <= 0) {
+        this.$bvModal.show('user-modal')
+      } else {
+        this.showModal = false
+        this.showMap = true
+        this.$nextTick(() => {
+          const mapElement = this.$refs.googleMapRef.$el
+          mapElement.scrollIntoView({ behavior: 'smooth' })
+        })
+      }
     },
     async fetchImage() {
       try {
@@ -104,21 +138,14 @@ export default {
         console.error('Unsplash Error:', error)
       }
     }
-  },
-  // Fetch background image when component is created
-  created() {
-    this.fetchImage()
-  },
-  components: {
-    'right-sidebar': RightSidebar,
-    GoogleMap
   }
 }
 </script>
 
 <style scoped>
 .home-container {
-  height: 100vh; /* Making it full height */
+  height: 100vh;
+  /* Making it full height */
   width: 100%;
   background-size: cover;
   background-repeat: no-repeat;
@@ -147,6 +174,7 @@ export default {
   background-color: #e751e4;
   border-color: #e751e4;
 }
+
 .button-style:active {
   background-color: #e751e4;
   border-color: #e751e4;
@@ -174,7 +202,7 @@ export default {
 }
 
 .search-box {
-  width: 30%;  /* or whatever width you prefer */
+  width: 30%;
+  /* or whatever width you prefer */
 }
-
 </style>
