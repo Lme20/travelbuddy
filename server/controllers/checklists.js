Lme20/travelbuddy

commit 185829473b1e2ad8e320dd0ff35e13eb94dc60c5

building settings page (mostly for dev&testing)

server/controllers/checklists.js
@@ -30,7 +30,7 @@ router.get('/api/checklists/:id', async (req, res) => {
         } else if (!req.body.owner) {
             res.status(400).send({ message: 'Owner missing' })
         } else {
-            res.status(200).send(checklist);
+            res.status(200).send({ 'checklists': checklist });
         }
     } catch (error) {
         res.status(500).send({ message: 'Error in GET checklists/id', error: error.message });
@@ -100,7 +100,7 @@ router.get('/api/users/:id/checklists', async (req, res) => {
         if (!user) {
             return res.status(404).send({message: "User not found"});
         }
-        res.send(user.checklists);
+        res.status(200).send({ 'checklists': user.checklists });
     } catch (error) {
         res.status(500).send({ message: 'Error in GET /users/id/checklists/', error: error.message });
     }
@@ -117,7 +117,7 @@ router.get('/api/users/:uid/checklists/:cid', async (req, res) => {
         if (!checklist) {
             return res.status(404).send({ message: "Checklist not found" });
         }
-        res.send(checklist);
+        res.status(200).send({ 'checklists': checklist });
     } catch (error) {
         res.status(500).send({ message: 'Error in GET /users/uid/checklists/cid', error: error.message });
     }

client/src/views/Settings.vue
@@ -1,10 +1,46 @@
 <template>
-    <div class="col-3">
-        <h1>Create new user</h1>
+  <div class="col-9">
+    <h1>Settings</h1>
+
+    <b-card-group columns>
+      <b-card header="Users">
         <b-button @click="contentToDisplay = 3">Create User</b-button>
-        <right-sidebar :contentToDisplay="contentToDisplay" />
-    </div>
-  </template>
+        <b-button variant="danger" @click="onDeleteUsers">Delete all</b-button>
+        <b-list-group>
+          <b-list-group-item v-for="user in users" :key="user._id">
+            {{ user.name }}
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+      <b-card header="Checklists">
+        <b-button variant="danger" @click="onDeleteChecklists">Delete all</b-button>
+        <b-list-group>
+          <b-list-group-item v-for="checklist in checklists" :key="checklist._id">
+            {{ checklist.title }}
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+      <b-card header="Journals">
+        <b-button variant="danger" @click="onDeleteJournals">Delete all</b-button>
+        <b-list-group>
+          <b-list-group-item v-for="journal in Journals" :key="journal._id">
+            {{ journal.title }}
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+      <b-card header="Locations">
+        <b-button variant="danger" @click="onDeleteLocations">Delete all</b-button>
+        <b-list-group>
+          <b-list-group-item v-for="location in locations" :key="location._id">
+            {{ location.placeName }}
+          </b-list-group-item>
+        </b-list-group>
+      </b-card>
+    </b-card-group>
+
+    <right-sidebar :contentToDisplay="contentToDisplay" />
+  </div>
+</template>
 
 <script>
 import { Api } from '@/Api'
@@ -13,14 +49,80 @@ import RightSidebar from '@/components/RightSidebar.vue'
 export default {
   data() {
     return {
+      users: [],
+      checklists: [],
+      journals: [],
+      locations: [],
       contentToDisplay: null,
       show: true
     }
+  },
+  mounted() {
+    this.getUsers()
+    this.getChecklists()
+    this.getJournals()
+    this.getLocations()
   },
   components: {
     'right-sidebar': RightSidebar
   },
   methods: {
+    getUsers() {
+      Api.get('/users')
+        .then(response => {
+          this.users = response.data.users
+        })
+        .catch(error => {
+          this.users = []
+          console.log(error)
+          //   TODO: display some error message instead of logging to console
+        })
+        .then(() => {
+          console.log('This runs every time after success or error.')
+        })
+    },
+    getChecklists() {
+      Api.get('/checklists')
+        .then(response => {
+          this.checklists = response.data.checklists
+        })
+        .catch(error => {
+          this.checklists = []
+          console.log(error)
+          //   TODO: display some error message instead of logging to console
+        })
+        .then(() => {
+          console.log('This runs every time after success or error.')
+        })
+    },
+    getJournals() {
+      Api.get('/journals')
+        .then(response => {
+          this.journals = response.data.journals
+        })
+        .catch(error => {
+          this.journals = []
+          console.log(error)
+          //   TODO: display some error message instead of logging to console
+        })
+        .then(() => {
+          console.log('This runs every time after success or error.')
+        })
+    },
+    getLocations() {
+      Api.get('/locations')
+        .then(response => {
+          this.locations = response.data.locations
+        })
+        .catch(error => {
+          this.locations = []
+          console.log(error)
+          //   TODO: display some error message instead of logging to console
+        })
+        .then(() => {
+          console.log('This runs every time after success or error.')
+        })
+    },
     onSubmit(event) {
       event.preventDefault()
       alert(JSON.stringify(this.form))
@@ -52,5 +154,4 @@ export default {
 }
 </script>
 
-  <style scoped>
-  </style>
+<style scoped></style>

server/controllers/users.js
@@ -22,7 +22,7 @@ router.post('/api/users', async (req, res) => {
 router.get('/api/users', async (req, res) => {
     try {
         const users = await User.find();
-        res.status(200).send(users);
+        res.status(200).send({ 'users': users });
     } catch (error) {
         res.status(500).send({ message: 'Error in GET /users', error: error.message });
     }

commit 8871b98b00acc78f91c84cd5a5be32faab2ad07f

resolved fault introduced in 185829473b1e2ad8e320dd0ff35e13eb94dc60c5

server/controllers/checklists.js
@@ -30,7 +30,7 @@ router.get('/api/checklists/:id', async (req, res) => {
         } else if (!req.body.owner) {
             res.status(400).send({ message: 'Owner missing' })
         } else {
-            res.status(200).send({ 'checklists': checklist });
+            res.status(200).send(checklist);
         }
     } catch (error) {
         res.status(500).send({ message: 'Error in GET checklists/id', error: error.message });
@@ -121,7 +121,7 @@ router.get('/api/users/:uid/checklists/:cid', async (req, res) => {
         if (!checklist) {
             return res.status(404).send({ message: "Checklist not found" });
         }
-        res.status(200).send({ 'checklists': checklist });
+        res.status(200).send(checklist);
     } catch (error) {
         res.status(500).send({ message: 'Error in GET /users/uid/checklists/cid', error: error.message });
     }